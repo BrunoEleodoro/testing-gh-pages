ee02daac45f04f92f214cc2f02a7ed116fa555df
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-07-20 00:47:25 -0300

    Updates

---
+++ precache-manifest.3de325b3a38415acd75653dc74ccfcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12f60f1c16bbc6e68bec6d77df61a33",
+    "url": "/testing-gh-pages/index.html"
+  },
+  {
+    "revision": "b809bb0ed5ecfae52cd2",
+    "url": "/testing-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/testing-gh-pages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b809bb0ed5ecfae52cd2",
+    "url": "/testing-gh-pages/static/js/main.b1e07bd6.chunk.js"
+  },
+  {
+    "revision": "1a499e863c32a38f19d4",
+    "url": "/testing-gh-pages/static/js/runtime~main.bdf85b51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
