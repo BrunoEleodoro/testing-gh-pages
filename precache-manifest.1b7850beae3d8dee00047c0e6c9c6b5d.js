b217cf578858bc3dfd8496997667ff46e75a4da4
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-07-25 15:35:49 -0300

    Updates

--- precache-manifest.1b7850beae3d8dee00047c0e6c9c6b5d.js
+++ precache-manifest.1b7850beae3d8dee00047c0e6c9c6b5d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfc8c92f0f1dbca90547dbc7e1ad41d3",
+    "revision": "8adce64763fdaeeb86d8575554aa5557",
     "url": "/testing-gh-pages/index.html"
   },
   {
-    "revision": "151120190ea99f54312d",
+    "revision": "906bac7281cb20b6e1ca",
     "url": "/testing-gh-pages/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "8edd0c0a57e9013666d6",
-    "url": "/testing-gh-pages/static/js/2.d03d7a43.chunk.js"
+    "revision": "e8773c6fc8cc680897ba",
+    "url": "/testing-gh-pages/static/js/2.a70fc3a4.chunk.js"
   },
   {
-    "revision": "151120190ea99f54312d",
-    "url": "/testing-gh-pages/static/js/main.031d254f.chunk.js"
+    "revision": "906bac7281cb20b6e1ca",
+    "url": "/testing-gh-pages/static/js/main.8bcce83c.chunk.js"
   },
   {
     "revision": "1a499e863c32a38f19d4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testing-gh-pages/precache-manifest.5fb2543d1c304bc5fae928aedde0bcce.js"
+  "/testing-gh-pages/precache-manifest.1b7850beae3d8dee00047c0e6c9c6b5d.js"
 );
 
 self.addEventListener('message', (event) => {
